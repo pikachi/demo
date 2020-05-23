67d4a80c6f6fcc8d953aff936f7fb12c146dc1ca
sanka <815474211@qq.com>
2020-05-23 19:10:04 +0800

    Revert "Revert "添加视图管理器""
    This reverts commit da1b5adb9b02733fb66897239b233778740f730c.

---
+++ script/View/ViewManager.ts
@@ -0,0 +1,97 @@
+import { View } from "./View";
+
+/**界面管理器 */
+export default class ViewManager {
+    /**是否关闭全部窗口 */
+    isCloseAllView = false;
+
+    /**缓存视图 */
+    viewPool = {};
+
+    /**累计叠加次数 */
+    addZindex = 0;
+
+    constructor() {
+        this.viewPool = {};
+        this.addZindex = 0;
+    }
+
+    /**
+     * 新建视图
+     * @param node 节点
+     * @param zIndex 层级
+     * @param isaddPersistRoot 是否为常驻节点
+     */
+    newView(node: cc.Node, zIndex: number, isaddPersistRoot: boolean) {
+        let view = new View(node, zIndex, isaddPersistRoot);
+        let cell: View = this.viewPool[node.name]
+        if (cell) {
+            cell.destroy();
+        }
+        this.viewPool[node.name] = view;
+    }
+
+
+    /**
+     * 打开视图
+     * @param _name 视图名字
+     */
+    openView(_name) {
+        this.addZindex++;
+        if (!this.viewPool[_name]) {
+            delete this.viewPool[_name];
+            console.log("没有这个窗口哦")
+            return
+        } else {
+            this.viewPool[_name].open(this.addZindex)
+        }
+    }
+
+    /**
+     * 关闭视图
+     * @param _name 视图名字
+     */
+    closeView(_name) {
+        if (!this.viewPool[_name]) {
+            console.log("没有这个窗口哦", _name)
+            delete this.viewPool[_name];
+            return
+        } else {
+            this.viewPool[_name].close();
+        }
+    }
+
+    /**
+     * 关闭所有视图
+     */
+    closeAllView() {
+        for (let name in this.viewPool) {
+            this.closeView(name);
+        }
+    }
+
+    /**
+     * 销毁视图
+     * @param _name 视图名字
+     */
+    destroyView(_name) {
+        if (!this.viewPool[_name]) {
+            delete this.viewPool[_name];
+            console.log("没有这个窗口哦", _name)
+            return
+        } else {
+            this.viewPool[_name].destroy();
+            delete this.viewPool[_name];
+        }
+    }
+
+    /**
+     * 销毁全部视图
+     */
+    destroyAllView() {
+        for (let name in this.viewPool) {
+            this.destroyView(name);
+        }
+    }
+
+}
